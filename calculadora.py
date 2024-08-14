f01d82edcbeba61a013b15b505128b329ec2b94c
José Gustavo <160790800+JoseCostS@users.noreply.github.com>
2024-08-14 15:48:35 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 class Calculadora:
     
     def somar(num1, num2):
-        return int(num1 + num2)
+        return int(num1) + int(num2)
   
     def maiorNumero(num1, num2):
         if num1 == num2:
@@ -10,4 +10,4 @@ class Calculadora:
 
     
     def diferenca(numero1, numero2):
-        return abs(numero1 - numero2)
+        return abs int(numero1) - int(numero2)
